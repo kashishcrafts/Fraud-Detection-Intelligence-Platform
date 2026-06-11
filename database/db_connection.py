b680c36bb4e3cf2092417d17669c1a574a644826
Kashishcrafts <ashaikh50304@gmail.com>
2026-06-11 22:47:57 +0530

    Fix db_connection Railway URL

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/fraud_detection_db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-=======
-from sqlalchemy import create_engine
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not found")
 
-DATABASE_URL = "postgresql://postgres:YOUR_PASSWORD@localhost:5432/fraud_detection_db"
-
-engine = create_engine(DATABASE_URL)
-
->>>>>>> d7cc06efc5da1142cb42abd2819e93cfde5d83bb
-print("Database Connected Successfully")
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
