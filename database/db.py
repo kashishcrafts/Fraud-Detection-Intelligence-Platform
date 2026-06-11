43577b81f128113519cfa4c284370ce21a174117
Kashishcrafts <ashaikh50304@gmail.com>
2026-06-11 21:39:40 +0530

    Fix db.py merge conflict

--- database/db.py
+++ database/db.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
@@ -11,10 +10,3 @@ engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
-=======
-from sqlalchemy import create_engine
-
-DATABASE_URL = "postgresql://postgres:cyber2026@postgres:5432/fraud_detection_db"
-
-engine = create_engine(DATABASE_URL)
->>>>>>> d7cc06efc5da1142cb42abd2819e93cfde5d83bb
